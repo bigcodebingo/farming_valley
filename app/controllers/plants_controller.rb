95b97b4407edcbc1d2acc6d63d3631cb7d4ac4f0
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-01-25 00:14:52 +0300

    add plants pages

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,48 @@
 class PlantsController < ApplicationController
-end
+    def index
+      @plants = Plant.all
+    end
+  
+    def show
+      @plant = Plant.find(params[:id])
+    end
+  
+    def new
+      @plant = Plant.new
+    end
+  
+    def edit
+      @plant = Plant.find(params[:id])
+    end
+  
+    def create
+      @plant = Plant.new(plant_params)
+      if @plant.save
+        redirect_to plants_path, notice: 'Plant successfully created!'
+      else
+        render :new
+      end
+    end
+  
+    def update
+      @plant = Plant.find(params[:id])
+      if @plant.update(plant_params)
+        redirect_to plant_path(@plant), notice: 'Plant was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @plant = Plant.find(params[:id])
+      @plant.destroy
+      redirect_to plants_path, notice: 'Plant was successfully deleted.'
+    end
+  
+    private
+  
+    def plant_params
+      params.require(:plant).permit(:plantid, :workerid, :name, :kind, :date_of_delivery)
+    end
+  end
+  
